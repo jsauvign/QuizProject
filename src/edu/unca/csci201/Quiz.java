153c77b22f1be746246f519025ce7d356017e487
KH037s <KH037s@KH037-05.uncanet.unca.edu>
2018-11-19 17:39:15 -0500

    Starting Files Quiz Project

---
+++ src/edu/unca/csci201/Quiz.java
@@ -0,0 +1,16 @@
+package edu.unca.csci201;
+
+public class Quiz {
+//I manage up to 25 Question objects
+	public void addQuestion(Question q) {
+	//This accepts a question object and adds it to it's list of questions
+	}
+	public double giveQuiz() {
+	//This presents each question, one by one, to the user and asks for their answer.  
+	//After each answer, it checks if the answer is correct and keeps track of the results.  
+	//Finally, it returns the score the user got.
+		return 0;
+	}
+
+
+}
---
+++ src/edu/unca/csci201/TrueFalseQuestion.java
@@ -0,0 +1,23 @@
+package edu.unca.csci201;
+
+public class TrueFalseQuestion implements Question {
+
+	@Override
+	public String getTheQuestionText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isCorrectAnswer(String answer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCorrectAnswer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
