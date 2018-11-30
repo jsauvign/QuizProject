fedc4ea1d6678860ed21d981b052c90226c0eb68
johns <johns@DESKTOP-U8P0FUO>
2018-11-30 16:47:11 -0500

    Updated Quiz and QuizTime.java

--- src/edu/unca/csci201/Quiz.java
+++ src/edu/unca/csci201/Quiz.java
@@ -4,55 +4,47 @@ import java.util.Scanner;
 
 //This class manages up to 25 Question objects
 public class Quiz {
-	static double grade = 0;
-	static int numWrong = 0;
+	private static double grade = 0;
 	static int numQuestions = 0;
-	static Question [] wrongQuestions = new Question[numWrong];
-	static Question [] questions;
+	private static Question[] questions = new Question[1]; //This array stores questions for the quiz
+	TrueFalseQuestion [] tfquestions;
 	
-	public Quiz(Question[] questions, double grade) {
-	Quiz.questions = questions;
-	Quiz.grade = grade;
+	public Quiz() {
+	//Quiz.questions = questions;
+	//Quiz.grade = grade;
 	}
 	
 //This accepts a question object and adds it to it's list of questions
-	public void addQuestion(Question q) {
-		questions[numQuestions] = q;
-		numQuestions ++;
+	public static void addQuestion(Question q) {
+	questions[numQuestions] = q;
+	numQuestions ++;
 		}
-
 	
 	
-	public double giveQuiz() {
+	public static int getNumberQuestions(){
+		return numQuestions;
+	}
+	
+	
 //This presents each question, one by one, to the user and asks for their answer.  
+	public double giveQuiz() {
 		Scanner scan = new Scanner(System.in);
-		
 		for(int i = 0; i < questions.length; i++ ) {
 			System.out.println(questions[i].getTheQuestionText());
 			String answer = scan.nextLine();
 
-			
 //After each answer, it checks if the answer is correct and keeps track of the results.  
 			if (questions[i].isCorrectAnswer(answer)){
 				grade++;
 			}
-//			else {
-//				wrongQuestions[numWrong] = questions[i];
-//				numWrong++;
-//				System.out.println("Sorry, the answer was " + questions[i].getCorrectAnswer());
-//			}
 			
 		}		
 
 //Finally, it returns the score the user got.
 		System.out.println("Congrats, You got " + grade + " out of " + questions.length + " correct!");
-		System.out.println("Here's what you missed: ");
-		System.out.println(wrongQuestions[numWrong]);
 		scan.close();
 		return grade;
-		
-
-		
+			
 	}
 
 
--- src/edu/unca/csci201/QuizTime.java
+++ src/edu/unca/csci201/QuizTime.java
@@ -1,18 +1,11 @@
 package edu.unca.csci201;
 
 public class QuizTime {
-	static double grade;
-	static String q1 = "1+1 = 2";
-	static String q2 = "1+1 = 9";
-	static TrueFalseQuestion [] tfQuestions = {
-			new TrueFalseQuestion(q1, "True"),
-			new TrueFalseQuestion(q2, "False")
-	};
-
 	
 	public static void main(String[] args) {
-		Quiz tfQuiz = new Quiz(tfQuestions, grade);
+		Question q1 = new TrueFalseQuestion ("a?", "a");
+		Quiz tfQuiz = new Quiz();	
+		Quiz.addQuestion(q1);
 		tfQuiz.giveQuiz();
 	}
-
 }
